a25dc51a0aee24eb1c5ae08a55526f5d1d9697db
danascape <danascape@gmail.com>
2022-11-27 19:01:56 +0530

    repository: Import getTop

--- repository/repository.sh
+++ repository/repository.sh
@@ -5,3 +5,21 @@
 #
 # SPDX-License-Identifier: Apache-2.0 license
 #
+
+function getTop() {
+    local TOPFILE=build/envsetup.sh
+    if [[ -f "$TOPFILE" ]] ; then
+        PWD= /bin/pwd
+    else
+        local HERE=$PWD
+        local T=
+        while [ \( ! \( -f $TOPFILE \) \) -a \( "$PWD" != "/" \) ]; do
+            \cd ..
+            T=`PWD= /bin/pwd -P`
+        done
+            \cd "$HERE"
+        if [ -f "$T/$TOPFILE" ]; then
+            echo "$T"
+        fi
+    fi
+}
