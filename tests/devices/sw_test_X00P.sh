3ad98d490fbd5e0dde327611ffdf06ddedd87143
danascape <danascape@gmail.com>
2023-01-10 03:26:59 +0530

    sw: test: Switch to kernel directory

--- tests/devices/sw_test_X00P.sh
+++ tests/devices/sw_test_X00P.sh
@@ -15,4 +15,5 @@ git clone --depth=1 https://github.com/stormbreaker-project/aarch64-linux-androi
 git clone --depth=1 https://github.com/stormbreaker-project/arm-linux-androideabi-4.9 $TC_DIR/gcc_32 >/dev/null 2>&1
 git clone --depth=1 -b aosp-11.0.5 https://github.com/sohamxda7/llvm-stable $TC_DIR/clang >/dev/null 2>&1
 export PATH="$HOME/tc/clang/bin:$HOME/tc/gcc/bin:$HOME/tc/gcc_32/bin:${PATH}"
+cd X00P
 bash $WORK_DIR/sw b X00P
