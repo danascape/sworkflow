0fbbe8c58aaec8f862f83685d93e76ee44baea63
Saalim Quadri <danascape@gmail.com>
2023-04-17 21:51:37 +0000

    test: Fix shellcheck warnings
    Issue :- https://github.com/danascape/sworkflow/issues/18
    
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

---
+++ tests/sw_test_X00P.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#
+# Copyright (C) 2019 Saalim Quadri (danascape)
+#
+# SPDX-License-Identifier: Apache-2.0 license
+#
+
+cd "$HOME" || exit 1
+mkdir tc
+export TC_DIR="$HOME/tc"
+export WORK_DIR="$HOME/sworkflow"
+git clone --depth 1 -b main https://github.com/stormbreaker-project/X00P
+git clone --depth=1 https://github.com/stormbreaker-project/aarch64-linux-android-4.9 "$TC_DIR"/gcc >/dev/null 2>&1
+git clone --depth=1 https://github.com/stormbreaker-project/arm-linux-androideabi-4.9 "$TC_DIR"/gcc_32 >/dev/null 2>&1
+git clone --depth=1 -b aosp-11.0.5 https://github.com/sohamxda7/llvm-stable "$TC_DIR"/clang >/dev/null 2>&1
+export PATH="$HOME/tc/clang/bin:$HOME/tc/gcc/bin:$HOME/tc/gcc_32/bin:${PATH}"
+cd X00P || exit 1
+bash "$WORK_DIR"/sw b X00P
