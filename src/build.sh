8481dd21f728293b4e806e839955a2513f5231ce
Saalim Quadri <danascape@gmail.com>
2023-04-17 22:05:15 +0000

    src: build: Fix shellcheck warnings
    Issue :- https://github.com/danascape/sworkflow/issues/18
    
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- src/build.sh
+++ src/build.sh
@@ -6,8 +6,8 @@
 # SPDX-License-Identifier: Apache-2.0 license
 #
 
-. $SW_SRC_DIR/src/build_vars.sh --source-only
-. $SW_SRC_DIR/src/sw_functions.sh --source-only
+. "$SW_SRC_DIR"/src/build_vars.sh --source-only
+. "$SW_SRC_DIR"/src/sw_functions.sh --source-only
 
 # Check if the kernel config already exists for a particular device.
 # This check is being performed to add support for official devices.
@@ -15,26 +15,27 @@ check_kernel() {
 	#	parse_build_arguments "$1"
 
 	local device
+	local device_config_dir
 	device="$1"
+	device_config_dir="device"
 	echo "Checking if kernel config exists for $device"
-	if [[ -n $(find -L $SW_SRC_DIR/device -maxdepth 2 -name "sworkflow.$device.config") ]]; then
-		for dir in device; do
-			for f in $(cd "$SW_SRC_DIR" && test -d $dir &&
-				find -L $SW_SRC_DIR -maxdepth 4 -name "sworkflow.$device.config" | sort); do
+	if [[ -n $(find -L "$SW_SRC_DIR"/device -maxdepth 2 -name "sworkflow.$device.config") ]]; then
+		for dir in $device_config_dir; do
+			for f in $(cd "$SW_SRC_DIR" && test -d "$dir" &&
+				find -L "$SW_SRC_DIR" -maxdepth 4 -name "sworkflow.$device.config" | sort); do
 				echo "sworkflow: Including $f"
 				. "$f" --source-only
 			done
 		done
 		echo "sworkflow: Config for $device found"
-	elif [[ -n $(find -L $(pwd) -maxdepth 2 -name "sworkflow.$device.config") ]]; then
-		for dir in device; do
-			for f in $(cd $(pwd) && test -d $dir &&
-				find -L $(pwd) -maxdepth 4 -name "sworkflow.$device.config" | sort); do
+	elif [[ -n $(find -L "$(pwd)" -maxdepth 2 -name "sworkflow.$device.config") ]]; then
+		for dir in $device_config_dir; do
+			for f in $(cd "$(pwd)" && test -d "$dir" &&
+				find -L "$(pwd)" -maxdepth 4 -name "sworkflow.$device.config" | sort); do
 				echo "sworkflow: Including $f"
 				. "$f" --source-only
 			done
 		done
-		echo $test
 		echo "sworkflow: sworkflow.$device.config found outside the tree"
 	else
 		echo "error: No config file found"
@@ -46,13 +47,13 @@ check_kernel() {
 
 kernel_build() {
 	device="$3"
-	check_kernel $device
+	check_kernel "$device"
 	echo "sworkflow: Starting Kernel Build!"
 
 	if [[ -z "$(command -v nproc)" ]]; then
-		parallel_threads=$(nproc --all)
+		parallel_threads="$(nproc --all)"
 	else
-		parallel_threads=$(grep -c ^processor /proc/cpuinfo)
+		parallel_threads="$(grep -c ^processor /proc/cpuinfo)"
 	fi
 
 	if ! is_kernel_root "$PWD"; then
@@ -66,31 +67,31 @@ kernel_build() {
 
 	if [[ -n "$cross_compile" ]]; then
 		cross_compile="CROSS_COMPILE=$cross_compile"
-		MAKE+=($cross_compile)
+		MAKE+=( "$cross_compile" )
 	fi
 
 	if [[ -n "$cross_compile_arm32" ]]; then
 		cross_compile_arm32="CROSS_COMPILE_ARM32=$cross_compile_arm32"
-		MAKE+=($cross_compile_arm32)
+		MAKE+=( "$cross_compile_arm32" )
 	fi
 
 	if [[ -n "$use_clang" ]]; then
 		cc="CC=clang"
 		clang_triple="CLANG_TRIPLE=aarch64-linux-gnu-"
-		MAKE+=($cc
-			$clang_triple)
+		MAKE+=( "$cc"
+			"$clang_triple")
 	fi
 
-	if [[ -f arch/arm64/configs/$kernel_defconfig ]]; then
+	if [[ -f arch/arm64/configs/"$kernel_defconfig" ]]; then
 		echo ""
 	else
 		echo "error: Defconfig not found!"
 		exit 22
 	fi
 
-	make O=out -j$parallel_threads ARCH=$kernel_arch "${MAKE[@]}" $kernel_defconfig
+	make O=out -j"$parallel_threads" ARCH="$kernel_arch" "${MAKE[@]}" "$kernel_defconfig"
 
-	command="make O=out -j$parallel_threads ARCH=$kernel_arch "${MAKE[@]}""
+	command="make O=out -j$parallel_threads ARCH=$kernel_arch ${MAKE[@]}"
 
 	start=$(date +%s)
 
@@ -104,7 +105,7 @@ kernel_build() {
 		else
 			if [[ -n "$dtbo_page_size" ]]; then
 				if [[ -n $dtbo_arch_path ]]; then
-					python3 $SW_SRC_DIR/utils/src/mkdtboimg.py create "out/arch/$kernel_arch/boot/dtbo.img" --page_size="$dtbo_page_size" $dtbo_arch_path
+					python3 "$SW_SRC_DIR"/utils/src/mkdtboimg.py create "out/arch/$kernel_arch/boot/dtbo.img" --page_size="$dtbo_page_size" "$dtbo_arch_path"
 				else
 					echo "error: kernel DTBO directory not defined!"
 					exit 22
