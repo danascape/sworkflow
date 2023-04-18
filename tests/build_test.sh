17435fed51f030911ec0caad1cc31bf15ff91f5e
Saalim Quadri <danascape@gmail.com>
2023-04-18 00:50:08 +0000

    sw: Update build test script
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- tests/build_test.sh
+++ tests/build_test.sh
@@ -10,10 +10,14 @@ cd "$HOME" || exit 1
 mkdir tc
 export TC_DIR="$HOME/tc"
 export WORK_DIR="$HOME/sworkflow"
+cd "$WORK_DIR" || exit 1
+bash install.sh
+source ~/.bashrc
+cd "$HOME" || exit 1
 git clone --depth 1 -b main https://github.com/stormbreaker-project/X00P
 git clone --depth=1 https://github.com/stormbreaker-project/aarch64-linux-android-4.9 "$TC_DIR"/gcc >/dev/null 2>&1
 git clone --depth=1 https://github.com/stormbreaker-project/arm-linux-androideabi-4.9 "$TC_DIR"/gcc_32 >/dev/null 2>&1
 git clone --depth=1 -b aosp-11.0.5 https://github.com/sohamxda7/llvm-stable "$TC_DIR"/clang >/dev/null 2>&1
 export PATH="$HOME/tc/clang/bin:$HOME/tc/gcc/bin:$HOME/tc/gcc_32/bin:${PATH}"
 cd X00P || exit 1
-bash "$WORK_DIR"/sw b X00P
+sw b X00P
