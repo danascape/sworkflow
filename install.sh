d6dec9c045cbfd815de3b3a2635888dca954e8c1
Saalim Quadri <danascape@gmail.com>
2023-04-18 00:42:18 +0000

    sw: install: Fix shellcheck warnings
    * A bit of cleanup as well
    
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- install.sh
+++ install.sh
@@ -6,11 +6,6 @@
 # SPDX-License-Identifier: Apache-2.0 license
 #
 
-## source code directories
-declare -r SRCDIR=$(pwd)
-
-source $SRCDIR/sw
-
 function safe_append()
 {
   if [[ $(grep -c -x "$1" "$2") == 0 ]]; then
@@ -22,14 +17,9 @@ update_path() {
 	local shellrc 
 	shellrc=${1:-'.bashrc'}
 	
-	IFS=':' read -ra ALL_PATHS <<< "$PATH"
-	for path in "${ALL_PATHS[@]}"; do
-		[[ "$path" -ef "$binpath" ]] && return
-	done
-	
 	# We are installing sworkflow in home directory
 	# Do not delete this directory
 	safe_append "PATH=${HOME}/sworkflow:\$PATH # sw" "${HOME}/${shellrc}"
 }
 
-update_path
+update_path "$@"
