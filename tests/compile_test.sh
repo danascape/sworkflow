a33a74f5e4235622ebf90c676c779b91344e291f
Saalim Quadri <danascape@gmail.com>
2023-07-31 16:44:07 +0530

    sw: tests: Clone and compile kernel in default working dir
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- tests/compile_test.sh
+++ tests/compile_test.sh
@@ -6,19 +6,14 @@
 # SPDX-License-Identifier: Apache-2.0 license
 #
 
-cd "$HOME" || exit 1
 mkdir tc
-export TC_DIR="$HOME/tc"
-export WORK_DIR="$HOME/sworkflow"
-cd "$WORK_DIR" || exit 1
-#bash install.sh
-#source ~/.bashrc
+export TC_DIR="$PWD/tc"
+export WORK_DIR="$PWD"
 
-cd "$HOME" || exit 1
 git clone --depth 1 -b master https://github.com/stormbreaker-project/linux-asus-x00p-3.18 X00P
 git clone --depth=1 https://github.com/stormbreaker-project/aarch64-linux-android-4.9 "$TC_DIR"/gcc >/dev/null 2>&1
 git clone --depth=1 https://github.com/stormbreaker-project/arm-linux-androideabi-4.9 "$TC_DIR"/gcc_32 >/dev/null 2>&1
 git clone --depth=1 -b aosp-11.0.5 https://github.com/sohamxda7/llvm-stable "$TC_DIR"/clang >/dev/null 2>&1
-export PATH="$HOME/tc/clang/bin:$HOME/tc/gcc/bin:$HOME/tc/gcc_32/bin:${PATH}"
+export PATH="$PWD/tc/clang/bin:$PWD/tc/gcc/bin:$PWD/tc/gcc_32/bin:${PATH}"
 cd X00P || exit 1
-"$HOME"/sworkflow/sw b X00P
+"$WORK_DIR"/sworkflow/sw b X00P
