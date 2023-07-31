8520b5f8ce6b60624fccf6fef2b9a00adab9814e
Saalim Quadri <danascape@gmail.com>
2023-07-31 13:34:10 +0530

    Doc: Use sphinx-book-theme
    Issue: [0], [1]
    
    [0]: https://github.com/danascape/sworkflow/issues/27
    [1]: https://github.com/danascape/sworkflow/issues/23
    
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
@@ -47,7 +47,12 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+try:
+    import sphinx_book_theme
+    html_theme = 'sphinx_book_theme'
+except ImportError:
+    sys.stdout.write('error: sw will assume the default theme - Alabaster\n')
+    html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
