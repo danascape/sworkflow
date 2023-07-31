c56bc86e4e14996b60762870a7099d2737aba6d7
Saalim Quadri <danascape@gmail.com>
2023-07-31 12:52:34 +0530

    sw: Reset color configs
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- src/sw_color.sh
+++ src/sw_color.sh
@@ -7,9 +7,16 @@
 #
 
 ## Color codes that will be used
-### Regular Colors
-export COLOR_BLACK='\033[0;30m'
-export COLOR_RED='\033[0;31m'
-export COLOR_GREEN='\033[0;32m'
-export COLOR_BLUE='\033[0;34m'
-export COLOR_WHITE='\033[0;37m'
+# red = errors, green = confirmations, cyan = warnings
+# bold blue = informational
+COLOR_RST=$(tput sgr0)
+COLOR_RED=$COLOR_RST$(tput setaf 1)
+COLOR_GREEN=$COLOR_RST$(tput setaf 2)
+COLOR_CYAN=$COLOR_RST$(tput setaf 6)
+COLOR_BOLD=$(tput bold)
+COLOR_BOLD_BLUE=$COLOR_RST$COLOR_BOLD$(tput setaf 4)
+
+export COLOR_RED
+export COLOR_GREEN
+export COLOR_CYAN
+export COLOR_BOLD_BLUE
